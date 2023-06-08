24d3a068949f67673590edc5657e2b0b89ae29d7
SubhojitGuin <subhojitguin2004@gmail.com>
2023-06-08 19:20:45 +0530

    Substitute todos.txt with FILEPATH in functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """Read the text file and return a list of to-do items"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_local, filepath='todos.txt'):
+def write_todos(todos_local, filepath=FILEPATH):
     """Write the to-do items list in the text file"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_local)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import functions
 import time
 
 
-now = time.strftime("%b %d,%Y %H:%M:%S.")
-print("It is",now)
+now = time.strftime("%b %d, %Y %H:%M:%S.")
+print("It is", now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
